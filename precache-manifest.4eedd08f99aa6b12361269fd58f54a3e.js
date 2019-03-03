d16f6b36c9c2c7638d92456293b400c258ea4bc6
woffpost <woffpost@gmail.com>
2019-03-03 14:06:41 +0200

    Updates

---
+++ precache-manifest.4eedd08f99aa6b12361269fd58f54a3e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "9b2d15347dbf2dd6be24",
+    "url": "/react-tutorial/static/js/runtime~main.9b2d1534.js"
+  },
+  {
+    "revision": "327e301a61cd8e07b207",
+    "url": "/react-tutorial/static/js/main.327e301a.chunk.js"
+  },
+  {
+    "revision": "2716c7985cfd03d7fd5d",
+    "url": "/react-tutorial/static/js/2.2716c798.chunk.js"
+  },
+  {
+    "revision": "327e301a61cd8e07b207",
+    "url": "/react-tutorial/static/css/main.623e1c3c.chunk.css"
+  },
+  {
+    "revision": "e88059056a77f810e219123155eff155",
+    "url": "/react-tutorial/index.html"
+  }
+];
